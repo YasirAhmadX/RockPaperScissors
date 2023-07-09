47619a7d4a9ef0fe516df911a1fd383a4f8d73ba
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2023-07-09 14:56:45 +0530

    BigUpdate Main.java
    Added MENU, Train option and Play option

--- Main.java
+++ Main.java
@@ -4,20 +4,61 @@ import java.util.Random;
 public class Main{  //Environment
     public static void main(String[] args) {
         System.out.println("Rock Paper Scissors Game");
-		Agent bot = new Agent(5,5,5);
-        Judge j = new Judge();
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of beads in each matchbox:  ");
+        int N = input.nextInt();
+
+		Agent bot = new Agent(N,N,N);  //Equal distribution
 		
-		System.out.println("Initial Tree:");
+		System.out.println("Initial Tree:"); 
 		bot.tree();
+
+        int choice;
+        do{
+            System.out.print("MENU\n1.Play \n2.Train Model\n3.View Tree\n4.About \n5.Quit\n::>  ");
+            choice = input.nextInt();
+            switch(choice){
+                case 1:
+                    input.nextLine();//clearing \n from input buffer
+                    System.out.print("Enter Player choice {R,P,S}: ");
+                    char oppChoice = input.nextLine().charAt(0);
+                    Play(bot,oppChoice);
+                    break;
+                case 2:
+                    System.out.print("Enter number of games: ");
+                    int n = input.nextInt();
+                    Train(bot,n);
+                    break;
+                case 3:
+                    bot.tree();
+                    break;
+                case 4:
+                    System.out.println("MENACE implementation of Rock Paper Scissors");
+                    System.out.println("By Yasir Ahmad.");
+                    break;
+                case 5:
+                    choice = 100;//to break while loop
+                    break;
+                default :
+                    System.out.println("Invalid input.");
+                    choice = 1; //to keep loop going.
+            }   
+        }while(choice>0 && choice<5);
+        input.close();
+
         
+    }
+
+    static void Train(Agent bot,int n){
         char[] plays = {'P','R','S'};
-        for(int i=0;i<100;i++)
+        for(int i=0;i<n;i++)
         {          
             System.out.println("\nPlay "+(i+1));
             Random r = new Random();
             char A = plays[r.nextInt(0,3)];
             char B = bot.play(A);
-            int result = j.verdict(A,B);
+            int result = Judge.verdict(A,B);
             System.out.println("JUDGE -> "+A+" vs "+B+": "+result);
             if(result == -1){
                 System.out.println("Punish bot");
@@ -31,6 +72,24 @@ public class Main{  //Environment
             System.out.println("Tree after "+i+" evolution:");
             bot.tree();
         }
+
+    }
+
+    static void Play(Agent bot,char A){
+
+        char B = bot.play(A);
+
+        int result = Judge.verdict(A,B);
+        System.out.println("Player: "+A+"\nComputer: "+B);
+        System.out.println("Judgement: "+((result==-1)?"Player wins":((result==0)?"Tie":"Computer Wins")));
+        if(result == -1){
+                System.out.println("Punish bot");
+                bot.punish(A,B);
+            }
+            else if(result == 1){
+                System.out.println("Reward bot");
+                bot.reward(A,B);
+            }
     }
 }
 
@@ -175,7 +234,7 @@ class Agent{
 }
 
 class Judge{
-    public int verdict(char A,char B){
+    public static int verdict(char A,char B){
         //-1 if A wins, 0 if draw, 1 if B wins
         if(A==B){
             return 0;
